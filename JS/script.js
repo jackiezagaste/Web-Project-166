03f3fa259bb20be6552227a3ac3ba6295a63ff8a
jackiezagaste <jaquelinezagaste@gmail.com>
2023-04-13 01:41:41 -0700

    mouseover
    added mouse over using JavaScript

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+function newPicture() {
+    document.getElementById("building").src="/Images/kitty.jpg";
+}
+
+function oldPicture() {
+    document.getElementById("building").src="/Images/building.jpg";
+}
+
+function newPicture1() {
+    document.getElementById("city").src="/Images/kitty.jpg";
+}
+function oldPicture1() {
+    document.getElementById("city").src="/Images/city.jpg";
+}
+
+function newPicture2() {
+    document.getElementById("fish").src="/Images/kitty.jpg";
+}
+function oldPicture2() {
+    document.getElementById("fish").src="/Images/fish.jpg";
+}
+
+function newPicture3() {
+    document.getElementById("rain").src="/Images/kitty.jpg";
+}
+
+function oldPicture3() {
+    document.getElementById("rain").src="/Images/rain.jpg";
+}
+
+function newPicture4() {
+    document.getElementById("fog").src="/Images/kitty.jpg";
+}
+
+function oldPicture4() {
+    document.getElementById("fog").src="/Images/fog.jpg";
+}
